ae209b53d793099f675a69817477dd84cf30a283
Valerie Lasso <165828539+MichelLasso@users.noreply.github.com>
2025-02-24 12:56:09 -0500

    feat :sparkles:

--- EncapsulamientoJava/src/encapsulamientojava/Producto.java
+++ EncapsulamientoJava/src/encapsulamientojava/Producto.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package encapsulamientojava;
 
-/**
- *
- * @author Uniminuto Tibu
- */
 public class Producto {
+    private String codigo;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto() {}
+    
+    public Producto(String codigo, String nombre, int cantidad, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Cantidad no puede ser negativa
+    public void verificarPrecio(double precio){
+        if (precio>0) {
+            this.precio= precio;
+            System.out.println("Precio agregado con éxito :)");
+        } else {
+            System.out.println("Ingrese el valor positivo");
+        }
+    }
+
+    
+    
     
 }
